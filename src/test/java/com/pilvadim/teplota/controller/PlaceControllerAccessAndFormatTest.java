2654553f0d941e442de8d3081eb917f2e928651e
vadimpilipenko <pilvadim@gmail.com>
2022-06-28 16:10:56 +0300

    rework for controller security

--- src/test/java/com/pilvadim/teplota/controller/PlaceControllerAccessAndFormatTest.java
+++ src/test/java/com/pilvadim/teplota/controller/PlaceControllerAccessAndFormatTest.java
@@ -1,23 +1,38 @@
 package com.pilvadim.teplota.controller;
 
+import com.pilvadim.teplota.TeplotaApplication;
+import com.pilvadim.teplota.model.Place;
+import com.pilvadim.teplota.model.Temperature;
+import com.pilvadim.teplota.repository.PlaceRepo;
+import com.pilvadim.teplota.service.PlaceService;
+import com.pilvadim.teplota.service.TemperatureService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@SpringBootTest
+@ContextConfiguration(classes= TeplotaApplication.class)
+@TestPropertySource("classpath:applicationTest.properties")
 class PlaceControllerAccessAndFormatTest {
 
     private final String PLACES = "/api/v1/places";
@@ -26,27 +41,34 @@ class PlaceControllerAccessAndFormatTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PlaceService ps;
+    @MockBean
+    private TemperatureService ts;
+
     MultiValueMap<String,String> paramsPeriod = new LinkedMultiValueMap<>();
     {
         paramsPeriod.add("start", "01.06.2022 00:00:00");
         paramsPeriod.add("end", "01.07.2022 00:00:00");
     }
 
-    String placeBodyUpdate = "{\"id\":6,\"name\": \"Moscow\",\"latitude\": 55.7522," +
+    String placeBodyUpdate = "{\"id\":2,\"name\": \"MoscowT\",\"latitude\": 55.7522," +
             "\"longitude\": 37.6156,\"period\": 10,\"enabled\": true}";
 
-    String placeBodyAdd = "{\"name\": \"Moscow\",\"latitude\": 55.7522," +
+    String placeBodyAdd = "{\"name\": \"MoscowT\",\"latitude\": 55.7522," +
             "\"longitude\": 37.6156,\"period\": 10,\"enabled\": true}";
 
     @Test
     @WithMockUser(username = "user", password = "pwd", roles = "USER")
     void getAllTestUser() throws Exception {
+        when( ts.getTemperaturesForPeriod(any(),any()) ).thenReturn(null);
         mockMvc.perform( MockMvcRequestBuilders.get(PLACES).params( paramsPeriod ))
                 .andExpect( status().isOk());
     }
 
     @Test
     void getAllTestNoUser() throws Exception {
+        when( ts.getTemperaturesForPeriod(any(),any()) ).thenReturn(null);
         mockMvc.perform( MockMvcRequestBuilders.get(PLACES).params( paramsPeriod ))
                 .andExpect( status().isOk());
     }
@@ -54,6 +76,7 @@ class PlaceControllerAccessAndFormatTest {
     @Test
     @WithMockUser(username = "user", password = "pwd", roles = "USER")
     void addPlaceTestUser() throws Exception  {
+        when( ps.addPlace( any(Place.class) ) ).thenReturn(1);
         mockMvc.perform( MockMvcRequestBuilders.post(PLACE)
                         .contentType( APPLICATION_JSON )
                         .content( placeBodyAdd ))
@@ -61,16 +84,8 @@ class PlaceControllerAccessAndFormatTest {
     }
 
     @Test
-    @WithMockUser(username = "user", password = "pwd", roles = "USER")
-    void addPlaceTestUserBadRequest() throws Exception  {
-        mockMvc.perform( MockMvcRequestBuilders.post(PLACE)
-                        .contentType( APPLICATION_JSON )
-                        .content( placeBodyUpdate ))
-                .andExpect( status().isBadRequest());
-    }
-
-    @Test
     void addPlaceTestNoUser() throws Exception  {
+        when( ps.addPlace( any(Place.class) ) ).thenReturn(1);
         mockMvc.perform( MockMvcRequestBuilders.post(PLACE)
                         .contentType( APPLICATION_JSON )
                         .content( placeBodyAdd ))
@@ -80,6 +95,7 @@ class PlaceControllerAccessAndFormatTest {
     @Test
     @WithMockUser(username = "user", password = "pwd", roles = "USER")
     void updatePlaceTestUser() throws Exception  {
+        when( ps.updatePlace( any(Place.class) ) ).thenReturn(1);
         mockMvc.perform( MockMvcRequestBuilders.put(PLACE)
                         .contentType( APPLICATION_JSON )
                         .content( placeBodyUpdate ))
@@ -87,16 +103,8 @@ class PlaceControllerAccessAndFormatTest {
     }
 
     @Test
-    @WithMockUser(username = "user", password = "pwd", roles = "USER")
-    void updatePlaceTestUserBadRequest() throws Exception  {
-        mockMvc.perform( MockMvcRequestBuilders.put(PLACE)
-                        .contentType( APPLICATION_JSON )
-                        .content( placeBodyAdd ))
-                .andExpect( status().isBadRequest());
-    }
-
-    @Test
     void updatePlaceTestNoUser() throws Exception  {
+        when( ps.updatePlace( any(Place.class) ) ).thenReturn(1);
         mockMvc.perform( MockMvcRequestBuilders.put(PLACE)
                         .contentType( APPLICATION_JSON )
                         .content( placeBodyUpdate ))
